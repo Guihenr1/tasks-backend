6e2b9d1901f346dd5abecdc0362c257a979a1206
Guilherme <guilherme.pomp@gmail.com>
2020-08-21 20:18:40 -0300

    Setup do projeto

---
+++ tasks.infra.crossCutting/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace tasks.infra.crossCutting
+{
+    public static class Bootstrapper
+    {
+        public static IServiceCollection AddSetupIoC (this IServiceCollection services, IConfiguration config) {
+            services.RegisterServices ();
+            services.AddRepositories ();
+            return services;
+        }
+
+        private static void RegisterServices (this IServiceCollection services) {
+            
+        }
+
+        private static void AddRepositories (this IServiceCollection services) {
+            
+        }
+    }
+}
